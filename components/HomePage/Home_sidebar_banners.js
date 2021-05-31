f698407609d8ebb1d0a871b071192663fa533c70
Ivan <ivan.ivanov@altius.mk>
2021-05-31 11:02:10 +0200

    dodadeni baneri

--- components/HomePage/Home_sidebar_banners.js
+++ components/HomePage/Home_sidebar_banners.js
@@ -1,31 +1,56 @@
 import React from 'react';
 
 const Home_sidebar_banners = () => {
-    return (
-        <div className="col-md-3 col-12 px-0">
-        <div className="col-12 p-0 sidebar-banner">
-          {' '}
-          <div className="banner banner-square">
-            <script type="text/javascript"></script>
-            <noscript>
-              <a
-                target="_blank"
-                href="http://adserver.mk/revive-adserver/www/delivery/ck.php?n=1ae9e48"
-              >
-                <img
-                  border="0"
-                  alt=""
-                  src="http://adserver.mk/revive-adserver/www/delivery/avw.php?zoneid=247&amp;n=1ae9e48"
-                />
-              </a>
-            </noscript>
-          </div>
+  return (
+    <div className="col-md-3 col-12 px-0">
+      <div className="col-12 p-0 sidebar-banner">
+        {' '}
+        <div className="banner banner-square">
+          <ins
+            data-revive-zoneid="414"
+            data-revive-id="0fdb51aa345f685c34f5913961f2f061"
+          ></ins>
+          <script
+            async
+            src="https://adserver.mk/revive-adserver/www/delivery/asyncjs.php"
+          ></script>
         </div>
-        <div className="col-12 p-0 sidebar-banner">banner</div>
-        <div className="col-12 p-0 sidebar-banner">banner</div>
-        <div className="col-12 p-0 sidebar-banner">banner</div>
       </div>
-    );
-  }
-  
-  export default Home_sidebar_banners;
+      <div className="col-12 p-0 sidebar-banner">
+        {' '}
+        <ins
+          data-revive-zoneid="414"
+          data-revive-id="0fdb51aa345f685c34f5913961f2f061"
+        ></ins>
+        <script
+          async
+          src="https://adserver.mk/revive-adserver/www/delivery/asyncjs.php"
+        ></script>
+      </div>
+      <div className="col-12 p-0 sidebar-banner">
+        {' '}
+        <ins
+          data-revive-zoneid="414"
+          data-revive-id="0fdb51aa345f685c34f5913961f2f061"
+        ></ins>
+        <script
+          async
+          src="https://adserver.mk/revive-adserver/www/delivery/asyncjs.php"
+        ></script>
+      </div>
+      <div className="col-12 p-0 sidebar-banner">
+        {' '}
+        <ins
+          data-revive-zoneid="414"
+          data-revive-id="0fdb51aa345f685c34f5913961f2f061"
+        ></ins>
+        <script
+          async
+          src="https://adserver.mk/revive-adserver/www/delivery/asyncjs.php"
+        ></script>
+      </div>
+    </div>
+  );
+};
+
+export default Home_sidebar_banners;
